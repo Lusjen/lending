2c60ca89dfdade784d3a2188bee1143882cfd040
Andrew Basiuk <a.basyuk4779@gmail.com>
2019-01-09 16:14:14 +0200

    dist

---
+++ dist/index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+
+	<meta charset="utf-8">
+	<!-- <base href="/"> -->
+
+	<title>title</title>
+	<meta name="description" content="">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	
+	<!-- Template Basic Images Start -->
+	<meta property="og:image" content="path/to/image.jpg">
+	<link rel="icon" href="img/favicon/favicon.ico">
+	<link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon-180x180.png">
+	<!-- Template Basic Images End -->
+	
+	<!-- Custom Browsers Color Start -->
+	<meta name="theme-color" content="#000">
+	<!-- Custom Browsers Color End -->
+	<!-- <link rel="stylesheet" href="css/libs/ion.rangeSlider.min.css">
+	<link rel="stylesheet" href="css/libs/ion.rangeSlider.skinHTML5.min.css"> -->
+	<link rel="stylesheet" href="css/main.min.css">
+
+
+</head>
+
+<body>
+	<div class="block-logo">
+		
+	</div>
+	<div class="block">
+		<div class="block__info info">
+			<h2 class="info__title title-lg-show"><span class="title-big">NEW YORK</span> Concept House</h2>
+			<h3 class="info__subtitle">АМЕРИКАНСЬКА ЯКІСТЬ. УКРАЇНСЬКА ЦІНА.</h3>
+			<p class="info__description">NEW YORK Concept House — це можливість дотримуватися американського стилю життя в самому центрі Києва.</p>
+			<ul class="info__list">
+				<li class="info__item">Унікальна архітектура</li>
+				<li class="info__item">Консьєрж-сервіс</li>
+				<li class="info__item">Розвинена інфраструктура навколо будинку</li>
+				<li class="info__item">Система «Розумний будинок»</li>
+				<li class="info__item">Зручна транспортна розв’язка</li>
+				<li class="info__item">Квартири з терасами</li>
+			</ul>
+			<ul class="info__list-house">
+				<li class="info__item-house">
+					Клас житла:<span class="info__item-value"> бізнес + </span>
+				</li>
+				<li class="info__item-house">
+					Кількість поверхів:<span class="info__item-value">22</span>
+				</li>
+				<li class="info__item-house">
+					Кількість квартир:<span class="info__item-value">225</span>
+				</li>
+				<li class="info__item-house">
+					Кількість паркомісць:<span class="info__item-value">21</span>
+				</li>
+			</ul>
+			<ul class="info__list-house">
+				<li class="info__item-house">
+					Загальна площа <br> комерційних приміщень: <span class="info__item-value">1 610,68 м2</span>
+				</li>
+			</ul>
+			<ul class="info__list-house">
+				<li class="info__item-house">
+					Площі квартир:
+				</li>
+				<li class="info__item-house">
+					1-к:<span class="info__item-value">41 – 59 м2</span>
+				</li>
+				<li class="info__item-house">
+					2-к:<span class="info__item-value">89,6 м2</span>
+				</li>
+				<li class="info__item-house">
+					3-к:<span class="info__item-value">91 – 163,5 м2</span>
+				</li>
+				<li class="info__item-house">
+					Пентхауси:<span class="info__item-value"> 87,4 – 204 м2</span>
+				</li>
+			</ul>
+			<ul class="info__list-house">
+				<li class="info__item-house">
+					Адреса:<span class="info__item-value">вул. Антоновича, 74, Голосіївський район</span>
+				</li>
+			</ul>
+
+		</div>
+
+		<div class="block__img img-infra">
+			<div class="img-infra__text">введено в експлуатацію</div>
+			<a href="#" class="img-infra__location">
+				<img src="img/location-pin.png" alt="location">
+			</a>
+			<a href="#" class="img-infra__logo">
+				<img src="img/new-york-logo.svg" alt="logo">
+			</a>
+			<div class="img-infra__images">
+				<img src="img/new_york.png" alt="New York">
+			</div>
+			
+			<div class="img-infra__button">
+				<a href="#" class="btn">
+					<span class="btn__text">ПЛАНУВАННЯ/ШАХМАТКА</span>
+				</a>
+				<a href="#" class="btn">
+					<span class="btn__text">ПЕРЕГЛЯНУТИ ГАЛЕРЕЮ</span>
+				</a>
+			</div>
+		</div>
+		<div class="info__title-tablet title-lg-hide">
+			<h2 class="info__title title-gray"><span class="title-big">NEW YORK</span> Concept House</h2>
+		</div>
+		
+	</div>
+
+	<!-- begin block -->
+		<div class="block block__reverse">
+			<div class="block__info info">
+				<h2 class="info__title title-lg-show"><span class="title-big">NEW YORK</span> Concept House</h2>
+				<h3 class="info__subtitle">АМЕРИКАНСЬКА ЯКІСТЬ. УКРАЇНСЬКА ЦІНА.</h3>
+				<p class="info__description">NEW YORK Concept House — це можливість дотримуватися американського стилю життя в самому центрі Києва.</p>
+				<ul class="info__list">
+					<li class="info__item">Унікальна архітектура</li>
+					<li class="info__item">Консьєрж-сервіс</li>
+					<li class="info__item">Розвинена інфраструктура навколо будинку</li>
+					<li class="info__item">Система «Розумний будинок»</li>
+					<li class="info__item">Зручна транспортна розв’язка</li>
+					<li class="info__item">Квартири з терасами</li>
+				</ul>
+				<ul class="info__list-house">
+					<li class="info__item-house">
+						Клас житла:<span class="info__item-value"> бізнес + </span>
+					</li>
+					<li class="info__item-house">
+						Кількість поверхів:<span class="info__item-value">22</span>
+					</li>
+					<li class="info__item-house">
+						Кількість квартир:<span class="info__item-value">225</span>
+					</li>
+					<li class="info__item-house">
+						Кількість паркомісць:<span class="info__item-value">21</span>
+					</li>
+				</ul>
+				<ul class="info__list-house">
+					<li class="info__item-house">
+						Загальна площа <br> комерційних приміщень: <span class="info__item-value">1 610,68 м2</span>
+					</li>
+				</ul>
+				<ul class="info__list-house">
+					<li class="info__item-house">
+						Площі квартир:
+					</li>
+					<li class="info__item-house">
+						1-к:<span class="info__item-value">41 – 59 м2</span>
+					</li>
+					<li class="info__item-house">
+						2-к:<span class="info__item-value">89,6 м2</span>
+					</li>
+					<li class="info__item-house">
+						3-к:<span class="info__item-value">91 – 163,5 м2</span>
+					</li>
+					<li class="info__item-house">
+						Пентхауси:<span class="info__item-value"> 87,4 – 204 м2</span>
+					</li>
+				</ul>
+				<ul class="info__list-house">
+					<li class="info__item-house">
+						Адреса:<span class="info__item-value">вул. Антоновича, 74, Голосіївський район</span>
+					</li>
+				</ul>
+
+			</div>
+			<div class="block__img img-infra">
+				<div class="img-infra__text img-infra__text_reverse">введено в експлуатацію</div>
+				<a href="#" class="img-infra__location img-infra__location_reverse">
+					<img src="img/location-pin.png" alt="location">
+				</a>
+				<a href="#" class="img-infra__logo img-infra__logo_reverse">
+					<img src="img/new-york-logo.png" alt="logo">
+				</a>
+				<div class="img-infra__images">
+					<img src="img/new_york.png" alt="New York">
+				</div>
+				
+				<div class="img-infra__button img-infra__button_reverse">
+					<a href="#" class="btn">
+						<span class="btn__text">ПЛАНУВАННЯ/ШАХМАТКА</span>
+					</a>
+					<a href="#" class="btn">
+						<span class="btn__text">ПЕРЕГЛЯНУТИ ГАЛЕРЕЮ</span>
+					</a>
+				</div>
+			</div>
+			<div class="info__title-tablet title-lg-hide">
+				<h2 class="info__title title-gray"><span class="title-big">NEW YORK</span> Concept House</h2>
+			</div>
+		</div>
+	<!-- end block -->
+
+	
+	<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+	<!-- <script src="js/scripts.min.js"></script> -->
+
+</body>
+</html>
